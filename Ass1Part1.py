96bf3acf1ae74f4f71b983df5d5dbaa3a0976990
Smokinghalo8 <ethanfbuckner@gmail.com>
2023-08-24 14:00:10 -0400

    Week 1 updated

--- Ass1Part1.py
+++ Ass1Part1.py
@@ -43,12 +43,49 @@ def part3():
 
 #Your company has shares of stock it would like to sell when their value exceeds a certain target price. 
 # Write a program that reads the target price and then reads the current stock price until it is at least the target price. 
-# Your program should read a sequence of floating-point values from standard input. 
+# Your program should read a sequence of floating-point values from standard input.         what
 # Upon reaching or exceeding the target price, your program should output a message that says "Shares can be sold now".
+
+
+# Your program should read a sequence of floating-point values from standard input. 
+# ^^ Does this mean an array or user input???
 
 
 def part4():
     CurrentValue=0
     targetValue = int(input(print("Target Price: ")))
     while CurrentValue<targetValue:
-        
+        CurrentValue = int(input(print("Current Price: ")))
+    print(f"Shares can now be sold! Current Price: {CurrentValue} Target Price: {targetValue}")
+
+
+def part4m2():
+    curr=[17,29,39,10,46,103,28,183]
+    target = int(input(print("Target Price: ")))
+    for n in curr:
+        if n >=target:
+            print(f"Shares can now be sold! Current Price: {curr} Target Price: {target}")
+        else:
+            print(f"Shares Cannot be Sold, Changing number; Currect Number in Curr: {n}")
+
+
+
+#At one college, the tuition for a full-time student is $8,000 per semester.
+#It has been announced that the tuition will increase by 3 percent each year for the next 5 years.
+# Write a program with a loop that displays the projected semester tuition amount for each of the next 5 years.
+
+
+def part5():
+    startingTuitionSemester = 8000
+    endAmount = 0
+    for n in range(0,10): #should be 10 passes
+            if(n%2==0 and n!=0):
+                #print(f"Going through the reminder flag pass: {n}") #debugFlag
+                startingTuitionSemester = startingTuitionSemester*1.003
+            endAmount +=startingTuitionSemester
+            #print(f"Pass Number: {n} Amount of money ended Right Now: {endAmount}")
+            print(f"Semester Number: {n} Price so far: {endAmount}")
+
+part5()
+
+
