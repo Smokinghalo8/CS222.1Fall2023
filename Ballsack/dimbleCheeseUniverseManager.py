afe03aa5c768a4c79d0f142669676d125d080106
Smokinghalo8 <ethanfbuckner@gmail.com>
2023-11-20 10:46:12 -0500

    adding more stuffs to the cheese universe holder - finished the split function with the arrays

--- Ballsack/dimbleCheeseUniverseManager.py
+++ Ballsack/dimbleCheeseUniverseManager.py
@@ -4,8 +4,19 @@ import os
 
 def sparkplug():
     tempDir=os.getcwd()
-    DirectorySaveFileList = os.listdir(tempDir+"\Ballsack\assets\universe.txt")
-    file1 = open(DirectorySaveFileList,"r")
-    print(file1.read())
+    DirectorySaveFileList = (tempDir+"\\Ballsack\\assets\\universe.txt")
+    print(DirectorySaveFileList)
+    with open(DirectorySaveFileList,"r") as UniverseFile:
+        LinesOfFile = str(UniverseFile.readlines())
+        ArrayWithInformation = LinesOfFile.split("-")
+    print()
+
+
+
+class Character:
+    def __init__(self):
+        pass
+
+
 
 sparkplug()
